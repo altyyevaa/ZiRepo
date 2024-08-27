a5889f1eb431ca38b9fda8e2b429d682155d0208
Aziza Altyyeva <altyyevaa@berea.edu>
2024-08-27 19:49:17 -0400

    Codes from chapter #6, #7

--- practice.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <fstream>
-
-//in_stream.open("myFile.txt");
-
-int a;
-cin >> a >> endl;
-cout << "Number is " << a << endl;
-
---
+++ week0/lem_0.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+//in_stream.open("myFile.txt");
+
+int main()
+{
+    int a;
+    cin >> a ;
+    cout << "Number is: " << a << endl;
+    return 0;
+}
+
